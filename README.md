c3376f34ae2779415c23a45bee3f8cd32fff9569
knakayama <knakayama.sh@gmail.com>
2015-06-28 11:20:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+tpm fast moving
+===============
+
+fast moving to tpm plugin directory with peco style
+
+# Requirements
+
+1. [tpm](https://github.com/tmux-plugins/tpm)
+1. [peco](https://github.com/peco/peco)
+1. [zsh](http://www.zsh.org/)
+
+# Install
+
+```zsh
+antigen knakayama/tpm-fast-moving
+antigen apply
+bindkey '^x^f' -tpm-fast-moving # or your favorite key
+```
+
+[MIT](https://github.com/knakayama/tpm-fast-moving/blob/master/LICENSE)
+
+# Author
+
+[knakayama](https://github.com/knakayama)
---
+++ tpm-fast-moving.plugin.zsh
@@ -0,0 +1,39 @@
+-tpm-fast-moving() {
+  local i
+  for i in "peco" "tmux"; do
+    if ! type "$i" &>/dev/null; then
+      cat <<EOT 1>&2
+$i does not found in your \$PATH.
+EOT
+    return 1
+    fi
+  done
+
+  if [[ -z "$TMUX_PLUGIN_MANAGER_PATH" ]]; then
+    cat <<'EOT' 1>&2
+$TMUX_PLUGIN_MANAGER_PATH is not set, tpm does not work properly on your machine?
+EOT
+  return 1
+  fi
+
+  cd $(tmux show-options -gqv @tpm_plugins \
+    | tr ' ' '\n' \
+    | cut -d/ -f2 \
+    | awk '/[:alnum:]/ {print}' \
+    | xargs -I% echo ${TMUX_PLUGIN_MANAGER_PATH}% \
+    | peco
+  )
+  zle accept-line
+  zle -R -c
+}
+
+autoload -Uz -- -tpm-fast-moving
+zle -N -- -tpm-fast-moving
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
